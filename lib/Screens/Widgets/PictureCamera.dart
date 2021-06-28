69b6b7940fe1b7d20a2adc9f7c75a1212a4ac789
tawatchai7034 <tawatchaisunanta@gmail.com>
2021-06-28 15:59:34 +0700

    test git push data

---
+++ lib/Screens/Widgets/PictureCamera.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// reference https://youtu.be/BAgLOAGga2o
+
+class Camerabutoon extends StatefulWidget {
+  const Camerabutoon({ Key? key }) : super(key: key);
+
+  @override
+  _CamerabutoonState createState() => _CamerabutoonState();
+}
+
+class _CamerabutoonState extends State<Camerabutoon> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
